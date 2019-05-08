5b71373f7c81aa08d64cbd6523510d90d1c58714
Christian Eder <christian.eder@zuehlke.com>
2019-05-08 08:48:53 +0200

    added cluster api controller

---
+++ AkkaIoTIngress/Controllers/ClusterController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AkkaIoTIngress.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Cluster")]
+    public class ClusterController : Controller
+    {
+        [HttpGet("machine")]
+        public async Task<string> Machine()
+        {
+            return Environment.MachineName;
+        }
+    }
+}
--- AkkaIoTIngress/Startup.cs
+++ AkkaIoTIngress/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Akka.Actor;
+﻿using System;
+using Akka.Actor;
 using AkkaIoTIngress.Actors.Ingress;
 using AkkaIoTIngress.Services;
 using Microsoft.AspNetCore.Builder;
@@ -30,13 +31,16 @@ namespace AkkaIoTIngress
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, IngressActorProvider ingressActorProvider)
         {
+            Console.WriteLine("Startup.Configure 1");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            Console.WriteLine("Startup.Configure 2");
             app.UseMvc();
 
+            Console.WriteLine("Startup.Configure 3");
             var ingressActor = ingressActorProvider.Get();
             ingressActor.Tell(new IngressActor.StartListening());
         }
